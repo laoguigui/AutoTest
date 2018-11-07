a1e4ac8c9c171828599b730231b3493a59b2037b
[zhouguili] <[zhouguili@treefinance.com.cn]>
2018-11-07 16:19:08 +0800

    自动化测试学习

--- chapter5/src/main/java/com/course/testng/multithread/MultiTreadOnXml.java
+++ chapter5/src/main/java/com/course/testng/multithread/MultiTreadOnXml.java
@@ -1,8 +1,20 @@
 package com.course.testng.multithread;
 
+import org.testng.annotations.Test;
+
 public class MultiTreadOnXml {
 
+    @Test
     public void test1(){
+        System.out.printf("Tread Id:%s%n",Thread.currentThread().getId());
 
+    }
+    @Test
+    public void test2(){
+        System.out.printf("Tread Id:%s%n",Thread.currentThread().getId());
+    }
+    @Test
+    public  void test3(){
+        System.out.printf("Tread Id: %s%n",Thread.currentThread().getId());
     }
 }
